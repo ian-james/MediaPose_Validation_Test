b7a61d87d0ffbcadef1fd315061eb56896d63242
Jamey F <jamey.fraser@gmail.com>
2023-06-23 20:35:20 -0300

    log forgotten

---
+++ src/log.py
@@ -0,0 +1,16 @@
+import logging
+
+# Debugging Levels
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+# INFO: Confirmation that things are working as expected.
+# WARNING: An indication that something unexpected happened or indicative of some problem in the near future(e.g. ‘disk space low’). The software is still working as expected.
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+# CRITICAL: A very serious error, indicating that the program itself may be unable to continue running.
+
+def set_log_level(level):
+    """Set the logging level based on the specified string"""
+    numeric_level = getattr(logging, level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {level}")
+    logging.basicConfig(level=numeric_level,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
